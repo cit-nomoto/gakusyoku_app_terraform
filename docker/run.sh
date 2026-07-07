3f4bdd2d4861e189016800f61f95384402a01888
kishsaver <tyurome@remote-develop.local>
2026-07-07 22:30:13 +0000

    [modify] gunicorn

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,9 @@ set -e
 # instance ディレクトリを作成する (SQLite のデータベースファイルの保存先)
 mkdir -p /app/instance
 
+#gunicornのinstall
+pip install 'gunicorn>=20.1'
+
 # Google Cloud Storage からデータベースファイルを復元する
 # `-if-replica-exists` フラグを指定すると、レプリカが存在する場合にのみ復元を行う
 # レプリカがあれば復元する（同一デプロイ内の再起動をまたいでデータを保持）。
@@ -21,4 +24,4 @@ fi
 
 # Google Cloud Storage にデータベースファイルをレプリケートしながら Gunicorn でアプリを起動する
 litestream replicate -config /etc/litestream.yml \
-  -exec "gunicorn --bind 0.0.0.0:8080 --workers 1 main:app"
+  -exec "gunicorn --bind 0.0.0.0:8080 --workers 1 main:create_app"
